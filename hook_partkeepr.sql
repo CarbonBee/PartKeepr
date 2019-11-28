2f17bbbbf08caadb556269ed773dc90492d06fab
Nathanaël Spriet <nathanael.spriet@gmail.com>
2019-11-28 17:34:23 +0100

    Change sql hook to trim and substring zeros

--- hook_partkeepr.sql
+++ hook_partkeepr.sql
@@ -1,25 +1,39 @@
+/*
+Don't forget to change the delimiter with
+mysql> delimiter //
+
+And change it back to ; with
+mysql> delimiter ;
+*/
+delimiter //
+
+DROP TRIGGER IF EXISTS before_insert_part//
+DROP TRIGGER IF EXISTS before_update_part//
+
 CREATE TRIGGER `before_insert_part` BEFORE INSERT ON `Part`
  FOR EACH ROW BEGIN
 DECLARE nextId int;
 DECLARE startUid varchar(50);
 
-SELECT SUBSTRING(categoryPath, 17, 1) FROM `PartCategory` 
+SELECT TRIM(LEADING '0' FROM SUBSTRING(SUBSTRING_INDEX(categoryPath, ' ', 4),17)) FROM `PartCategory`
 WHERE NEW.category_id = PartCategory.id INTO startUid;
 
 SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = 'Part' INTO nextId;
 	SET New.internalPartNumber = CONCAT_WS('-', startUid, LPAD(nextId, 4, '0'));
-end
+end//
 
 CREATE TRIGGER `before_update_part` BEFORE UPDATE ON `Part`
  FOR EACH ROW BEGIN
 DECLARE startUid varchar(50);
 
-SELECT SUBSTRING(categoryPath, 17, 1) FROM `PartCategory` 
+SELECT TRIM(LEADING '0' FROM SUBSTRING(SUBSTRING_INDEX(categoryPath, ' ', 4),17)) FROM `PartCategory`
 WHERE NEW.category_id = PartCategory.id INTO startUid;
 if New.internalPartNumber = "" THEN
 	SET New.internalPartNumber = CONCAT_WS('-', startUid, LPAD(NEW.Id, 4, '0'));
 END IF;
-end
+end//
+
+delimiter ;
 
 /*
 query to re-run uid computation
